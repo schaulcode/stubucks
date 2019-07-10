ed725ebf46e332ff0cfbe6707a7e7fd48276de21
Schaul <schaulrotenberg@gmail.com>
2019-07-10 23:54:11 +0100

    installed mongodb, read from database and display

--- routes/menu.js
+++ routes/menu.js
@@ -1,9 +1,46 @@
 var express = require('express');
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert")
 var router = express.Router();
 
-/* GET users listing. */
+const url = 'mongodb://localhost:27017';
+const dbName = 'stubucks';
+
+
+var products,categories;
+/* GET Menu page. */
 router.get('/', function(req, res, next) {
-  res.render('menu', {title : 'Menu'});
-});
+  var client = new MongoClient(url);
+  try {
+    client.connect((err)=>{
+      assert.equal(null,err);
+      console.log("Connected successfully to server");
+    
+      const db = client.db(dbName);
+  
+      // db.collection('products').find().toArray((err,result)=>{
+      //   if (err) throw err
+      //   products = result;
+      //   console.log(products);
+      //   res.render('menu', {title : 'Menu', products : products});
+      // });
+      (async ()=>{
+        products = await db.collection('products').find().toArray()
+        categories = await db.collection('categories').find().toArray()
+        res.render('menu', {title : 'Menu', products : products, categories : categories});
+        client.close()
+      })();
+    });
+  } catch (err) {
+    
+  }
+ 
+    
+  
+    // client.close();
+})
+
+  
+
 
 module.exports = router;
